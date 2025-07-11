e08b6370f56888c466e87d63205fe5fd763bb738
Lokendra Gupta <lokendra.marwarpay@gmail.com>
2025-07-11 12:57:51 +0530

    feat: enable CORS with specific settings for all origins and methods

--- server/app.js
+++ server/app.js
@@ -11,7 +11,14 @@ import messageRoutes from './routes/message.routes.js';
 const app = express();
 connectDB();
 
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+
+app.options('*', cors());
 app.use(express.json());
 
 // Routes
